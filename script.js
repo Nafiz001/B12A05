3e9809812a66f7926ca8669b189bbf81d6c87cd7
Md. Nafiz Ahmed <nafizahmed000@gmail.com>
2025-08-26 22:16:08 +0600

    added function for alert when clicked call

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ document.querySelectorAll(".heart-btn").forEach(function(element) {
         
     });
 });
-
-document.getElementById("call-btn").addEventListener("click", function() {
-    alert("Calling "+ this.parentElement.parentElement.querySelector("h1").innerText)+" ";
+document.querySelectorAll(".call-btn").forEach(function(element) {
+    element.addEventListener("click", function() {
+    alert("📞 Calling "+ this.parentElement.parentElement.querySelectorAll("h1")[0].innerText+" "+this.parentElement.parentElement.querySelectorAll("h1")[1].innerText);
+    });
 });
