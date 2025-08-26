091b2b1aeb466ea255f9319b6c02d2180d4c3f77
Md. Nafiz Ahmed <nafizahmed000@gmail.com>
2025-08-26 22:25:04 +0600

    added function for coin decrease after every call

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 let hearts = 0;
-document.querySelectorAll(".heart-btn").forEach(function(element) {
-    element.addEventListener("click", function() {
+const data=[];
+document.querySelectorAll(".heart-btn").forEach(function (element) {
+    element.addEventListener("click", function () {
         hearts += 1;
         document.getElementById("heart-count").innerText = hearts;
-        
+
     });
 });
-document.querySelectorAll(".call-btn").forEach(function(element) {
-    element.addEventListener("click", function() {
-    alert("📞 Calling "+ this.parentElement.parentElement.querySelectorAll("h1")[0].innerText+" "+this.parentElement.parentElement.querySelectorAll("h1")[1].innerText);
+document.querySelectorAll(".call-btn").forEach(function (element) {
+    element.addEventListener("click", function () {
+        if (document.getElementById("coin-count").innerText >= 20) {
+            alert("📞 Calling " + this.parentElement.parentElement.querySelectorAll("h1")[0].innerText + " " + this.parentElement.parentElement.querySelectorAll("h1")[1].innerText);
+            document.getElementById("coin-count").innerText = document.getElementById("coin-count").innerText - 20;
+            data.push({
+                name: this.parentElement.parentElement.querySelectorAll("h1")[0].innerText,
+                number: this.parentElement.parentElement.querySelectorAll("h1")[1].innerText,
+                time: new Date().toLocaleString()
+            });
+        }
+        else {
+            alert("❌ Not enough coins to make a call. You need at least 20 coins.");
+        }
+
     });
 });
